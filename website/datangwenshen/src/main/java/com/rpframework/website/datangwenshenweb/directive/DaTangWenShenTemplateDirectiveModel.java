bd9396e9cf23eeb012e35ee4f4eb3b81842ceadf
rplees <rplees.i.ly@gmail.com>
2015-05-21 17:20:49 +0800

    datws

---
+++ website/datangwenshen/src/main/java/com/rpframework/website/datangwenshenweb/directive/DaTangWenShenTemplateDirectiveModel.java
@@ -0,0 +1,62 @@
+package com.rpframework.website.datangwenshenweb.directive;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.rpframework.core.freemarker.directive.BaseTemplateDirectiveModel;
+import com.rpframework.core.freemarker.directive.DirectiveUtils;
+import com.rpframework.core.utils.SpringUtils;
+import com.rpframework.utils.Pager;
+import com.rpframework.website.datangwenshen.service.PictureService;
+
+import freemarker.core.Environment;
+import freemarker.template.ObjectWrapper;
+import freemarker.template.TemplateDirectiveBody;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateModel;
+
+@Component("datangwenshen")
+public class DaTangWenShenTemplateDirectiveModel extends BaseTemplateDirectiveModel {
+	final Logger logger = LoggerFactory.getLogger(getClass());
+	static final String GET_PIC_LIST = "get_pic_list";
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public void execute(Environment env, Map params, TemplateModel[] loopVars,
+			TemplateDirectiveBody body) throws TemplateException, IOException {
+		
+		
+		String cmd = DirectiveUtils.getString("cmd", params);
+		Map paramWarp = new HashMap(params);
+		boolean pass = true;
+		if(StringUtils.isBlank(cmd)) {
+			
+		} else if(StringUtils.equals(cmd, GET_PIC_LIST)) {
+			int source = DirectiveUtils.getInt("source", params);
+			int type = DirectiveUtils.getInt("type", params);
+			PictureService pictureService = SpringUtils.getBean(PictureService.class);
+			
+			String pagerString = DirectiveUtils.getString("pagerString", params);
+			Integer pageSize = DirectiveUtils.getInt("pageSize", params);
+			Pager pager = Pager.convertStringToPager(pagerString);
+			pager.setPageSize(pageSize);
+			pager.getSearchMap().put("source", String.valueOf(source));
+			pager.getSearchMap().put("type", String.valueOf(type));
+			pictureService.getPager(pager);
+			
+			paramWarp.put("m_pager", ObjectWrapper.DEFAULT_WRAPPER.wrap(pager));
+		}
+		
+		Map origWarp = DirectiveUtils.addParamsToVariable(env, paramWarp);
+		if(pass && body != null) {
+			body.render(env.getOut());
+		}
+		DirectiveUtils.removeParamsFromVariable(env, paramWarp, origWarp);
+	}
+}
